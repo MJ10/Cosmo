3d5640e678d19e8e5cfe4e8339dfde7a4669e027
MJ10 <mokshjn00@gmail.com>
2017-03-13 23:04:45 +0530

    fix search hint icon

--- app/src/main/java/io/mokshjn/cosmo/activities/SearchActivity.java
+++ app/src/main/java/io/mokshjn/cosmo/activities/SearchActivity.java
@@ -1,25 +1,44 @@
 package io.mokshjn.cosmo.activities;
 
 import android.app.SearchManager;
+import android.app.SharedElementCallback;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.support.annotation.TransitionRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.StyleSpan;
 import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
+import android.transition.TransitionSet;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.CheckedTextView;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.util.List;
+
 import butterknife.BindDimen;
 import butterknife.BindInt;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.mokshjn.cosmo.R;
 import io.mokshjn.cosmo.adapters.SongListAdapter;
+import io.mokshjn.cosmo.transitions.CircularReveal;
+import io.mokshjn.cosmo.utils.ImeUtils;
+import io.mokshjn.cosmo.utils.TransitionUtils;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -58,6 +77,9 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
         setupSearch();
+
+        onNewIntent(getIntent());
+        setupTransitions();
     }
 
     private void setupSearch() {
@@ -77,4 +99,109 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if(intent.hasExtra(SearchManager.QUERY)) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (!TextUtils.isEmpty(query)) {
+                searchView.setQuery(query, false);
+                searchFor(query);
+            }
+        }
+    }
+
+    @Override
+    public void onEnterAnimationComplete() {
+        // focus the search view once the enter transition finishes
+        searchView.requestFocus();
+        ImeUtils.showIme(searchView);
+    }
+
+    void clearResults() {
+        TransitionManager.beginDelayedTransition(container, getTransition(R.transition.auto));
+        results.setVisibility(View.GONE);
+        progress.setVisibility(View.GONE);
+        fab.setVisibility(View.GONE);
+        confirmSaveContainer.setVisibility(View.GONE);
+        resultsScrim.setVisibility(View.GONE);
+        setNoResultsVisibility(View.GONE);
+    }
+
+    Transition getTransition(@TransitionRes int transitionId) {
+        Transition transition = transitions.get(transitionId);
+        if (transition == null) {
+            transition = TransitionInflater.from(this).inflateTransition(transitionId);
+            transitions.put(transitionId, transition);
+        }
+        return transition;
+    }
+
+    void setNoResultsVisibility(int visibility) {
+        if (visibility == View.VISIBLE) {
+            if (noResults == null) {
+                noResults = (TextView) ((ViewStub)
+                        findViewById(R.id.stub_no_search_results)).inflate();
+                noResults.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        searchView.setQuery("", false);
+                        searchView.requestFocus();
+                        ImeUtils.showIme(searchView);
+                    }
+                });
+            }
+            String message = String.format(
+                    getString(R.string.no_search_results), searchView.getQuery().toString());
+            SpannableStringBuilder ssb = new SpannableStringBuilder(message);
+            ssb.setSpan(new StyleSpan(Typeface.ITALIC),
+                    message.indexOf('“') + 1,
+                    message.length() - 1,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            noResults.setText(ssb);
+        }
+        if (noResults != null) {
+            noResults.setVisibility(visibility);
+        }
+    }
+
+
+    private void searchFor(String query) {
+        clearResults();
+        progress.setVisibility(View.VISIBLE);
+        ImeUtils.hideIme(searchView);
+        searchView.clearFocus();
+    }
+
+    @OnClick({ R.id.scrim, R.id.searchback })
+    protected void dismiss() {
+        // clear the background else the touch ripple moves with the translation which looks bad
+        searchBack.setBackground(null);
+        finishAfterTransition();
+    }
+
+    private void setupTransitions() {
+        // grab the position that the search icon transitions in *from*
+        // & use it to configure the return transition
+        setEnterSharedElementCallback(new SharedElementCallback() {
+            @Override
+            public void onSharedElementStart(
+                    List<String> sharedElementNames,
+                    List<View> sharedElements,
+                    List<View> sharedElementSnapshots) {
+                if (sharedElements != null && !sharedElements.isEmpty()) {
+                    View searchIcon = sharedElements.get(0);
+                    if (searchIcon.getId() != R.id.searchback) return;
+                    int centerX = (searchIcon.getLeft() + searchIcon.getRight()) / 2;
+                    CircularReveal hideResults = (CircularReveal) TransitionUtils.findTransition(
+                            (TransitionSet) getWindow().getReturnTransition(),
+                            CircularReveal.class, R.id.results_container);
+                    if (hideResults != null) {
+                        hideResults.setCenter(new Point(centerX, 0));
+                    }
+                }
+            }
+        });
+    }
+
 }
