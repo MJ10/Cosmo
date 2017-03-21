c7fd721bddedcb53504d1aabcc07ea25d2b56a51
MJ10 <mokshjn00@gmail.com>
2017-03-21 23:05:54 +0530

    added nowplaying activities

--- app/src/main/java/io/mokshjn/cosmo/activities/NowPlayingActivity.java
+++ app/src/main/java/io/mokshjn/cosmo/activities/NowPlayingActivity.java
@@ -1,8 +1,35 @@
 package io.mokshjn.cosmo.activities;
 
+import android.app.Activity;
+import android.app.UiModeManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import io.mokshjn.cosmo.helpers.LogHelper;
+import io.mokshjn.cosmo.tv.TvPlaybackActivity;
+
 /**
  * Created by moksh on 19/3/17.
  */
 
-public class NowPlayingActivity {
+public class NowPlayingActivity extends Activity {
+    private static final String TAG = LogHelper.makeLogTag(NowPlayingActivity.class);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LogHelper.d(TAG, "onCreate");
+        Intent newIntent;
+        UiModeManager uiModeManager = (UiModeManager) getSystemService(UI_MODE_SERVICE);
+        if (uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION) {
+            LogHelper.d(TAG, "Running on a TV Device");
+            newIntent = new Intent(this, TvPlaybackActivity.class);
+        } else {
+            LogHelper.d(TAG, "Running on a non-TV Device");
+            newIntent = new Intent(this, MainActivity.class);
+        }
+        startActivity(newIntent);
+        finish();
+    }
 }
---
+++ app/src/main/java/io/mokshjn/cosmo/tv/TvPlaybackActivity.java
@@ -0,0 +1,8 @@
+package io.mokshjn.cosmo.tv;
+
+/**
+ * Created by moksh on 21/3/17.
+ */
+
+public class TvPlaybackActivity {
+}
