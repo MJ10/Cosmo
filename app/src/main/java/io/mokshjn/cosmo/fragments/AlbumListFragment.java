c0f1b5eedd6ec57d4904ddf900e5a73823d5b7dd
MJ10 <mokshjn00@gmail.com>
2017-04-09 12:17:27 +0530

    implement material-grid guidelines

--- app/src/main/java/io/mokshjn/cosmo/fragments/AlbumListFragment.java
+++ app/src/main/java/io/mokshjn/cosmo/fragments/AlbumListFragment.java
@@ -72,7 +72,7 @@ public class AlbumListFragment extends Fragment implements AlbumListAdapter.albC
         startActivity(intent,
                 ActivityOptions.makeSceneTransitionAnimation(getActivity(),
                         Pair.create(v.findViewById(R.id.ivAlbumArt), getString(R.string.transition_album_art)),
-                        Pair.create(v.findViewById(R.id.cvAlbum), getString(R.string.transition_album_bg))).toBundle());
+                        Pair.create(v.findViewById(R.id.albumText), getString(R.string.transition_album_bg))).toBundle());
     }
 
     @Override
