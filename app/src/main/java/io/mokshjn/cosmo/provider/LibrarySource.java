9506b9500358644fe112e31f8a526d507bfe0440
Moksh Jain <mokshjn00@gmail.com>
2017-06-15 16:06:20 +0530

    add album reading method

--- app/src/main/java/io/mokshjn/cosmo/provider/LibrarySource.java
+++ app/src/main/java/io/mokshjn/cosmo/provider/LibrarySource.java
@@ -48,4 +48,25 @@ public class LibrarySource implements MusicProviderSource {
 
         return tracks.iterator();
     }
+
+    public Iterator<MediaMetadataCompat> albums() {
+        ArrayList<MediaMetadataCompat> album = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI;
+        String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
+        Cursor cursor = resolver.query(uri, null, null, null, sortOrder);
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                MediaMetadataCompat mediaMetadataCompat = new MediaMetadataCompat.Builder()
+                        .putString(MediaMetadataCompat.METADATA_KEY_TITLE, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AlbumColumns.ALBUM)))
+                        .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AlbumColumns.ARTIST)))
+                        .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AlbumColumns.ALBUM_ID)))
+                        .putString(MediaMetadataCompat.METADATA_KEY_NUM_TRACKS, cursor.getString(cursor.getColumnIndex(MediaStore.Audio.AlbumColumns.NUMBER_OF_SONGS)))
+                        .build();
+                album.add(mediaMetadataCompat);
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+
+        return album.iterator();
+    }
 }
