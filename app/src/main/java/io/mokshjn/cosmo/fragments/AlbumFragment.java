6cd72a1da92b4640ebe217f844960d5781a86eb1
Moksh Jain <mokshjn00@gmail.com>
2017-06-16 17:06:07 +0530

    new album fragment for loading albums from  MediaBrowser

---
+++ app/src/main/java/io/mokshjn/cosmo/fragments/AlbumFragment.java
@@ -0,0 +1,206 @@
+package io.mokshjn.cosmo.fragments;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.util.Log;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.mokshjn.cosmo.R;
+import io.mokshjn.cosmo.activities.AlbumActivity;
+import io.mokshjn.cosmo.adapters.AlbumAdapter;
+import io.mokshjn.cosmo.helpers.LogHelper;
+import io.mokshjn.cosmo.helpers.MediaIDHelper;
+
+/**
+ * Created by moksh on 16/6/17.
+ */
+
+public class AlbumFragment extends android.support.v4.app.Fragment implements AlbumAdapter.albClickListener {
+    private static final String TAG = LogHelper.makeLogTag(SongsFragment.class);
+
+    private static final String ARG_MEDIA_ID = "media_id";
+
+    private FastScrollRecyclerView rvAlbumList;
+    private ArrayList<MediaBrowserCompat.MediaItem> albumList = new ArrayList<>();
+    private AlbumAdapter adapter;
+
+    private final MediaControllerCompat.Callback mMediaControllerCallback =
+            new MediaControllerCompat.Callback() {
+                @Override
+                public void onMetadataChanged(MediaMetadataCompat metadata) {
+                    super.onMetadataChanged(metadata);
+                    if (metadata == null) {
+                        return;
+                    }
+                    LogHelper.d(TAG, "Received metadata change to media ",
+                            metadata.getDescription().getMediaId());
+                    adapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onPlaybackStateChanged(@NonNull PlaybackStateCompat state) {
+                    super.onPlaybackStateChanged(state);
+                    LogHelper.d(TAG, "Received state change: ", state);
+                    adapter.notifyDataSetChanged();
+                }
+            };
+    private final MediaBrowserCompat.SubscriptionCallback mSubscriptionCallback =
+            new MediaBrowserCompat.SubscriptionCallback() {
+                @Override
+                public void onChildrenLoaded(@NonNull String parentId,
+                                             @NonNull List<MediaBrowserCompat.MediaItem> children) {
+                    Log.d(TAG, parentId);
+                    try {
+                        albumList.clear();
+                        for (MediaBrowserCompat.MediaItem item : children) {
+                            albumList.add(item);
+                        }
+                        adapter.setAlbums(albumList);
+                        adapter.notifyDataSetChanged();
+                    } catch (Throwable t) {
+                        LogHelper.e(TAG, "Error on childrenloaded", t);
+                    }
+                }
+
+                @Override
+                public void onError(@NonNull String id) {
+                    LogHelper.e(TAG, "browse fragment subscription onError, id=" + id);
+                    Toast.makeText(getActivity(), R.string.error_loading_media, Toast.LENGTH_LONG).show();
+                }
+            };
+    private String mMediaId;
+    private SongsFragment.MediaFragmentListener mMediaFragmentlistener;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onAttach(Activity context) {
+        super.onAttach(context);
+        mMediaFragmentlistener = (SongsFragment.MediaFragmentListener) context;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_song_list, container, false);
+        rvAlbumList = (FastScrollRecyclerView) rootView.findViewById(R.id.rvSongList);
+        rvAlbumList.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        adapter = new AlbumAdapter(getContext());
+        rvAlbumList.setAdapter(adapter);
+        adapter.setClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MediaBrowserCompat mediaBrowser = mMediaFragmentlistener.getMediaBrowser();
+
+        if (mediaBrowser.isConnected()) {
+            onConnected();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // fetch browsing information to fill the listview:
+        MediaBrowserCompat mediaBrowser = mMediaFragmentlistener.getMediaBrowser();
+
+        if (mediaBrowser.isConnected()) {
+            onConnected();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList("songs", albumList);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        MediaBrowserCompat mediaBrowser = mMediaFragmentlistener.getMediaBrowser();
+        if (mediaBrowser != null && mediaBrowser.isConnected() && mMediaId != null) {
+            mediaBrowser.unsubscribe(mMediaId);
+        }
+        MediaControllerCompat controller = getActivity()
+                .getSupportMediaController();
+        if (controller != null) {
+            controller.unregisterCallback(mMediaControllerCallback);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mMediaFragmentlistener = null;
+    }
+
+    public String getMediaId() {
+        Bundle args = getArguments();
+        if (args != null) {
+            return args.getString(ARG_MEDIA_ID);
+        }
+        return null;
+    }
+
+    public void onConnected() {
+        if (isDetached()) {
+            return;
+        }
+
+        mMediaId = MediaIDHelper.MEDIA_ID_MUSICS_BY_ALBUM;
+        Log.d(TAG, "onConnected: " + mMediaId);
+
+        mMediaFragmentlistener.getMediaBrowser().unsubscribe(mMediaId);
+
+        mMediaFragmentlistener.getMediaBrowser().subscribe(mMediaId, mSubscriptionCallback);
+
+        // Add MediaController callback so we can redraw the list when metadata changes:
+        MediaControllerCompat controller = getActivity()
+                .getSupportMediaController();
+        if (controller != null) {
+            controller.registerCallback(mMediaControllerCallback);
+        }
+    }
+
+    @Override
+    public void onAlbumClick(View v, int pos) {
+        Intent intent = new Intent(getActivity(), AlbumActivity.class);
+        Log.d(TAG, "onAlbumClick: " + albumList.get(pos).getMediaId());
+        intent.putExtra("albumID", MediaIDHelper.extractAlbumID(albumList.get(pos).getMediaId()));
+        startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(getActivity(),
+                        Pair.create(v.findViewById(R.id.ivAlbumArt), getString(R.string.transition_album_art)),
+                        Pair.create(v.findViewById(R.id.albumText), getString(R.string.transition_album_bg))).toBundle());
+    }
+}
