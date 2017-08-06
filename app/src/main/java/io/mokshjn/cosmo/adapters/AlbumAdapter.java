6dc8e5b43fc54372d1c1a339558f6acd6334ed79
Moksh Jain <mokshjn00@gmail.com>
2017-08-06 15:34:02 +0530

    fix palette

--- app/src/main/java/io/mokshjn/cosmo/adapters/AlbumAdapter.java
+++ app/src/main/java/io/mokshjn/cosmo/adapters/AlbumAdapter.java
@@ -2,7 +2,6 @@ package io.mokshjn.cosmo.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,10 +11,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.target.ImageViewTarget;
+import com.github.florent37.glidepalette.GlidePalette;
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 
 import java.util.ArrayList;
@@ -51,34 +47,15 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         MediaItem alb = albumList.get(position);
-        if (alb != null) {
-            holder.tvAlbumTitle.setText(alb.getDescription().getTitle());
-            holder.tvArtist.setText(alb.getDescription().getSubtitle());
-            Glide.with(context)
-                    .load(alb.getDescription().getIconUri())
-                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                    .crossFade()
-                    .into(new ImageViewTarget<GlideDrawable>(holder.ivAlbumArt) {
-                        @Override
-                        protected void setResource(GlideDrawable resource) {
-                            holder.ivAlbumArt.setImageDrawable(resource.getCurrent());
-                            Palette palette = Palette.from(((GlideBitmapDrawable) resource.getCurrent()).getBitmap()).generate();
-                            Palette.Swatch vibrant = palette.getVibrantSwatch();
-                            if (vibrant != null) {
-                                holder.cvAlbum.setBackgroundColor(vibrant.getRgb());
-                                holder.tvAlbumTitle.setTextColor(vibrant.getTitleTextColor());
-                                holder.tvArtist.setTextColor(vibrant.getBodyTextColor());
-                            } else {
-                                Palette.Swatch muted = palette.getDarkVibrantSwatch();
-                                if (muted != null) {
-                                    holder.cvAlbum.setBackgroundColor(muted.getRgb());
-                                    holder.tvAlbumTitle.setTextColor(muted.getTitleTextColor());
-                                    holder.tvArtist.setTextColor(muted.getBodyTextColor());
-                                }
-                            }
-                        }
-                    });
-        }
+        if (alb == null) return;
+        holder.tvAlbumTitle.setText(alb.getDescription().getTitle());
+        holder.tvArtist.setText(alb.getDescription().getSubtitle());
+        Glide.with(context)
+                .load(alb.getDescription().getIconUri())
+                .listener(GlidePalette.with(alb.getDescription().getIconUri().toString())
+                        .use(GlidePalette.Profile.MUTED_DARK)
+                        .intoBackground(holder.cvAlbum))
+                .into(holder.ivAlbumArt);
     }
 
     @Override
